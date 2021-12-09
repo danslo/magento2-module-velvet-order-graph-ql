385b9c815a3cd5003330d47033e24ead02b64639
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-09 19:29:41 +0100

    refactor grid

--- Model/Resolver/Grid/ItemTransformer.php
+++ Model/Resolver/Grid/ItemTransformer.php
@@ -6,9 +6,10 @@ namespace Danslo\VelvetOrderGraphQl\Model\Resolver\Grid;
 
 use Danslo\VelvetGraphQl\Api\EntityTransformerInterface;
 use Magento\Directory\Model\Currency;
+use Magento\Framework\Model\AbstractModel;
 use Magento\Store\Model\System\Store as SystemStore;
 
-class EntityTransformer implements EntityTransformerInterface
+class ItemTransformer implements ItemTransformerInterface
 {
     private Currency $currency;
     private array $currencyFormatFields;
@@ -29,7 +30,7 @@ class EntityTransformer implements EntityTransformerInterface
         return $this->store->getStoreName($storeId);
     }
 
-    public function transform(array $data): array
+    public function transform(AbstractModel $model, array $data): array
     {
         foreach ($this->currencyFormatFields as $field) {
             if (!isset($data[$field])) {
